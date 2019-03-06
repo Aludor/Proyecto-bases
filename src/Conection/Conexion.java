5122c2f8c5283b02bb7a53f65e479f677d81a1b7
fenrique21 <fenrique.21@hotmail.com>
2019-03-05 21:13:52 -0600

    comprobacion de usuario

---
+++ src/Conection/Conexion.java
@@ -0,0 +1,26 @@
+
+package Conection;
+
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Conexion {
+   
+    private static Connection conexion;
+     public Conexion(){}
+    public Connection ConectarBaseDatos(){
+           
+        try {
+
+             conexion = DriverManager.getConnection("jdbc:mariadb://localhost:3306/surtifiestas","root","Fercho.21");
+             System.out.println("conexion correcta");
+        } catch (SQLException ex) {
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+        }
+       return conexion;
+    }
+    
+    //public static Connection getConnection(){ return conexion; }
+    
+}
